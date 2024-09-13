fb2f6520684f85444a416951a0473c50f07f8e75
Eric <176455136+bonShonjon@users.noreply.github.com>
2024-09-13 21:41:13 +1000

    Add javascript calculator functionality

---
+++ script.js
@@ -0,0 +1,131 @@
+const inputDisplay = document.querySelector(".subdisplay__input");
+const expressionDisplay = document.querySelector(".subdisplay__expression");
+
+let inputString = "";
+let numberA = "";
+let numberB = "";
+let operator = "";
+let numberAnswer = "";
+
+const btnList = document.querySelectorAll(".button");
+btnList.forEach((btn) => btn.addEventListener("click", onClick));
+
+function onClick(e) {
+  if (e.target.className.includes("button--special")) {
+    onClickSpecial(e);
+  } else if (e.target.className.includes("button--numerical")) {
+    onClickNumerical(e);
+  } else if (e.target.className.includes("button--operator")){
+    onClickOperator(e);
+  }
+}
+
+function onClickSpecial(e) {
+  switch (e.target.textContent){
+    case "C":
+      inputString = "";
+      numberA = "";
+      numberB = "";
+      operator = "";
+      numberAnswer = "";
+      updateInputDisplay();
+      updateExpressionDisplay();
+      break;
+    case "=":
+      numberB = inputString;
+      resolveEquation();
+      //stops operation if error detected
+      if (inputString.includes("error")) {
+        inputString = "";
+        return
+      }
+      inputString = numberAnswer;
+      updateInputDisplay();
+      updateExpressionDisplay();
+      inputString = ""
+      numberA = numberAnswer;
+      numberB = "";
+      operator = "";
+
+  }
+}
+
+function onClickNumerical(e) {
+  //conditions to only allow one decimal point
+  if (e.target.textContent !== "·" || !inputString.includes("·")) {
+    inputString += e.target.textContent;
+    updateInputDisplay();
+    updateExpressionDisplay();
+  }
+}
+
+function onClickOperator(e) {
+  //if no number has been inputted, only "-" can be pressed to make number negative
+  if (!inputString.length && e.target.textContent === "-"){
+    inputString += "-";
+    updateInputDisplay();
+  //remove negative if no number inputted yet
+  } else if (inputString === "-" && e.target.textContent === "-") {
+    inputString = "";
+    updateInputDisplay();
+  //fill first number and operator
+  } else if (!numberA) {
+    numberA = inputString;
+    inputString = "";
+    operator = e.target.textContent;
+    updateExpressionDisplay();
+  //fill second number, resolve equation, restart with new operator
+  } else if (!numberB) {
+    numberB = inputString;
+    resolveEquation();
+    //stop operation if error detected
+    if (inputString.includes("error")) {
+      inputString = "";
+      return
+    }
+    updateExpressionDisplay();
+    inputString = numberAnswer;
+    updateInputDisplay();
+    inputString = "";
+    numberA = numberAnswer;
+    numberB = "";
+    operator = e.target.textContent;
+
+  }
+}
+
+function resolveEquation() {
+  switch(operator) {
+    case "+":
+      numberAnswer = +numberA + +numberB;
+      break;
+    case "-":
+      numberAnswer = +numberA - +numberB;
+      break;
+    case "×":
+      numberAnswer = +numberA * +numberB;
+      break;
+    case "÷":
+      //output error to screen and clear all values if divide by zero
+      if (+numberB === 0){
+        inputString = "Div zero error";
+        numberA = "";
+        numberB = "";
+        operator = "";
+        numberAnswer = ""
+        updateInputDisplay();
+        updateExpressionDisplay();
+      } else {
+      numberAnswer = +numberA / +numberB;
+      }
+      break;
+  } 
+}
+
+function updateInputDisplay() {
+  inputDisplay.textContent = inputString;
+}
+
+function updateExpressionDisplay() {
+  expressionDisplay.textContent = `${numberA} ${operator} ${numberB}`;
+}
