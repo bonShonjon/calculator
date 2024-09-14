73a08a5def9d50dfc76eb5c31e5a5db97f498ba4
Eric <176455136+bonShonjon@users.noreply.github.com>
2024-09-14 14:11:04 +1000

    Retooled the calculator logic

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+//define DOM elements for displays
 const inputDisplay = document.querySelector(".subdisplay__input");
 const expressionDisplay = document.querySelector(".subdisplay__expression");
+//define DOM elements for buttons
+const btnList = document.querySelectorAll(".button");
+btnList.forEach((btn) => btn.addEventListener("click", onClick));
 
+//define variables
 let inputString = "";
+let expressionString = "";
+let displayString = "";
 let numberA = "";
 let numberB = "";
 let operator = "";
 let numberAnswer = "";
 
-const btnList = document.querySelectorAll(".button");
-btnList.forEach((btn) => btn.addEventListener("click", onClick));
-
+//logic based on button class
 function onClick(e) {
   if (e.target.className.includes("button--special")) {
     onClickSpecial(e);
@@ -20,10 +25,20 @@ function onClick(e) {
   }
 }
 
+//logic for "C", "←", and "="
 function onClickSpecial(e) {
-  switch (e.target.textContent){
+  switch(e.target.textContent) {
+    //pressing "←" remove last character from input string
+    case "←":
+      inputString = inputString.slice(0,-1);
+      displayString = inputString;
+      updateInputDisplay();
+      break;
+    //pressing "C" clears all variables and displays
     case "C":
       inputString = "";
+      expressionString = "";
+      displayString = "";
       numberA = "";
       numberB = "";
       operator = "";
@@ -34,74 +49,69 @@ function onClickSpecial(e) {
     case "=":
       numberB = inputString;
       resolveEquation();
-      //stops operation if error detected
-      if (inputString.includes("error")) {
-        inputString = "";
-        return
-      }
-      inputString = numberAnswer;
-      updateInputDisplay();
+      displayString = numberAnswer;
+      handleError();
+      expressionString = `${numberA} ${operator} ${numberB}`;
       updateExpressionDisplay();
-      inputString = ""
-      numberA = numberAnswer.toString();
-      numberB = "";
-      operator = "";
-      break;
-    case "←":
-      inputString = inputString.slice(0,-1);
       updateInputDisplay();
-  }
-}
+      break;
 
+  }
+  }
+
+//logic for numbers
 function onClickNumerical(e) {
-  //conditions to only allow one decimal point
-  if (e.target.textContent !== "·" || !inputString.includes("·")) {
-    inputString += e.target.textContent;
-    updateInputDisplay();
-    updateExpressionDisplay();
+  //only allow one decimal point
+  if (e.target.textContent === "." && inputString.includes(".")) {
+    return
   }
+  inputString += e.target.textContent;
+  displayString = inputString;
+  updateInputDisplay();
+  updateExpressionDisplay();
 }
 
+//logic for operators
 function onClickOperator(e) {
-  //if no number has been inputted, only "-" can be pressed to make number negative
-  if (!inputString.length && e.target.textContent === "-" && numberAnswer === ""){
-    inputString += "-";
-    updateInputDisplay();
-  //remove negative if no number inputted yet
-  } else if (inputString === "-" && e.target.textContent === "-") {
-    inputString = "";
-    updateInputDisplay();
-  //fill first number and operator
-  } else if (!operator) {
-    //populate input string if performing operation after pressing equals
-    if (!inputString.length) {
-      inputString = numberAnswer;
+  //only allow negation when input string is empty
+  if (!inputString.length) {
+    if (e.target.textContent === "-") {
+      inputString += "-";
+      displayString = inputString;
+      updateInputDisplay();
     }
-    numberA = inputString;
-    inputString = "";
-    operator = e.target.textContent;
-    updateExpressionDisplay();
-  //fill second number, resolve equation, restart with new operator
-  } else if (!numberB) {
+  } else {
+    //When no numberA, only populate numberA
+    if (!numberA.length) {
+      operator = e.target.textContent;
+      numberA = inputString;
+      inputString = "";
+      expressionString = `${numberA} ${operator} ${numberB}`;
+      updateExpressionDisplay();
+    } else {
+    //If numberA is present, resolve existing equation
     numberB = inputString;
     resolveEquation();
-    //stop operation if error detected
-    if (inputString.includes("error")) {
-      inputString = "";
-      return
-    }
+    displayString = numberAnswer;
+    handleError();
+    expressionString = `${numberA} ${operator} ${numberB}`;
     updateExpressionDisplay();
-    inputString = numberAnswer;
     updateInputDisplay();
-    inputString = "";
-    numberA = numberAnswer.toString();
-    numberB = "";
+    //Use answer as numberA for next equation
+    numberA = numberAnswer;
     operator = e.target.textContent;
-
+    inputString = "";
+    expressionString = `${numberAnswer} ${operator}`
+    }
   }
+
 }
 
 function resolveEquation() {
+  //Do not operate if not every field is full
+  if (numberA === "" || numberB === "" || operator === "") {
+    return
+  }
   switch(operator) {
     case "+":
       numberAnswer = +numberA + +numberB;
@@ -110,29 +120,36 @@ function resolveEquation() {
       numberAnswer = +numberA - +numberB;
       break;
     case "×":
-      numberAnswer = +numberA * +numberB;
+      numberAnswer = +numberA * +numberB; 
       break;
     case "÷":
-      //output error to screen and clear all values if divide by zero
-      if (+numberB === 0){
-        inputString = "Div zero error";
-        numberA = "";
-        numberB = "";
-        operator = "";
-        numberAnswer = ""
-        updateInputDisplay();
-        updateExpressionDisplay();
+      if (numberB = "0") {
+        numberAnswer = "Div zero error"
       } else {
-      numberAnswer = +numberA / +numberB;
+        numberAnswer = +numberA / +numberB
       }
       break;
-  } 
+  }
+  numberAnswer = numberAnswer.toString();
 }
 
 function updateInputDisplay() {
-  inputDisplay.textContent = inputString;
+  inputDisplay.textContent = displayString;
 }
 
 function updateExpressionDisplay() {
-  expressionDisplay.textContent = `${numberA} ${operator} ${numberB}`;
+  expressionDisplay.textContent = expressionString;
+}
+
+function handleError() {
+  //if present, display error and clear all variables
+  if (numberAnswer.includes("error")) {
+    inputString = "";
+    expressionString = "";
+    displayString = numberAnswer;
+    numberA = "";
+    numberB = "";
+    operator = "";
+    numberAnswer = "";
+  }
 }
